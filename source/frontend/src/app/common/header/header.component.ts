28bdab7c606db726c0ed050fb0ddf7b0bfeed568
Sedina02 <sipos.edina02@gmail.com>
2022-06-15 09:06:22 +0200

    footer, header, datas first steps

--- source/frontend/src/app/common/header/header.component.ts
+++ source/frontend/src/app/common/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { faBowlFood } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faCake } from '@fortawesome/free-solid-svg-icons';
+import { faBreadSlice} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  faCoffee = faCoffee;
+  faHouseChimney = faHouseChimney;
+  faBowlFood = faBowlFood;
+  faUtensils = faUtensils;
+  faCake = faCake;
+  faBreadSlice = faBreadSlice;
 
   constructor() { }
 
